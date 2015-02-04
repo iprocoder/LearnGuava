a43d3fb026dc4da35f9c52b4c2381b92928d3e9d
iprocoder <iprocoder@163.com>
2015-02-04 19:39:56 +0800

    Optional Preconditions

---
+++ src/main/java/guava/PreconditionsTester.java
@@ -0,0 +1,48 @@
+package guava;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Created by acnightly on 2/4/15.
+ */
+public class PreconditionsTester {
+    public static void main(String[] args) {
+        PreconditionsTester tester = new PreconditionsTester();
+        try {
+            System.out.println(tester.sqrt(-3.0));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println(tester.sum(null, 3));
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println(tester.getValue(6));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public double sqrt(double input) throws IllegalArgumentException {
+        Preconditions.checkArgument(input > 0.0,
+                "Illegal Argument passed: Negative value %s.", input);
+        return Math.sqrt(input);
+    }
+
+    public int sum(Integer a, Integer b) {
+        a = Preconditions.checkNotNull(a,
+                "Illegal Argument passed: First parameter is Null.");
+        b = Preconditions.checkNotNull(b,
+                "Illegal Argument passed: Second parameter is Null.");
+        return a + b;
+    }
+
+    public int getValue(int input) {
+        int[] data = {1, 2, 3, 4, 5};
+        Preconditions.checkElementIndex(input, data.length,
+                "Illegal Argument passed: Invalid index.");
+        return 0;
+    }
+}
